75bb23e796c9b286b039c1182be865264c38bcb6
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-05-30 12:10:17 -0500

    draw some text

--- main.asm
+++ main.asm
@@ -7,6 +7,8 @@ section '.text' executable
   extrn WindowShouldClose
   extrn BeginDrawing
   extrn EndDrawing
+  extrn DrawText
+  extrn ClearBackground
 
 _start:
   mov rdi, 800
@@ -17,7 +19,16 @@ _start:
 
   ze_loop:
   call BeginDrawing
+    mov rdi, [BLACK]
+    call ClearBackground
+    mov rdi, title
+    mov rsi, 400
+    mov rdx, 300
+    mov rcx, 20
+    mov r8d, [WHITE]
+    call DrawText
   call EndDrawing
+
   call WindowShouldClose
   test rax, rax
   jz ze_loop
@@ -30,4 +41,15 @@ _start:
 
 section '.data' writeable
   title: db "foo bar baz", 0
+  BLACK:
+    db 0x00 ;r
+    db 0x00 ;g
+    db 0x00 ;b
+    db 0xFF ;a
+  WHITE:
+    db 0xFF ;r
+    db 0xFF ;g
+    db 0xFF ;b
+    db 0xFF ;a
+
 section '.note.GNU-stack'
