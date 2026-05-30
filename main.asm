118c2d7cc1eb8aa1e9450d42b13908122dd26145
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-05-30 11:48:44 -0500

    open and close window

--- main.asm
+++ main.asm
@@ -1,7 +1,22 @@
-section .text
-  global _start
+format ELF64
+
+section '.text' executable
+  public _start
+  extrn InitWindow
+  extrn CloseWindow
 
 _start:
-  mov rdi, 255
-  mov rax, 60
+  mov rdi, 800
+  mov rsi, 600
+  mov rdx, title
+  call InitWindow
+
+  call CloseWindow
+
+  mov rax, 231
+  mov rdi, 0
   syscall
+
+section '.data' writeable
+  title: db "foo bar baz", 0
+section '.note.GNU-stack'
