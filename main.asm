b5d34b13a0c1e5a931fac50236347aab18a63569
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-06-24 09:44:16 -0500

    remove unneeded paddle field

--- main.asm
+++ main.asm
@@ -94,12 +94,10 @@ section '.data' writeable
   left_paddle:
     dd 10 ;x
     dd 10 ;y
-    db 0  ;input (0=none, 1=up, 2=down)
     db 5  ;move speed
   right_paddle:
     dd 10 ;x
     dd 10 ;y
-    db 0  ;input (0=none, 1=up, 2=down)
     db 5  ;move speed
   ball:
     dd 10 ;pos x
